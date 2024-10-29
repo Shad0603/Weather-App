bcc1bf825c89a1f748e9649784d70e8405e3397c
Shad <shossain@jaguarlandrover.com>
2024-10-29 14:51:22 +0000

    Weather search by coordinates feature added

--- utils/weatherdata.js
+++ utils/weatherdata.js
@@ -1,7 +1,11 @@
+// This file acts as a utility module that handles the interaction with the OpenWeatherMap API.
+// It is responsible for data fetching as well as some error handling
+
 const axios = require('axios');
 
 const openWeatherMap = {
     BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+    COORDS_BASE_URL: "https://api.openweathermap.org/data/2.5/weather?",
     SECRET_KEY: "a7500de537763c63c3568504ba59830c"
 }
 
@@ -36,5 +40,32 @@ const weatherData = async (address, callback) => {
     }
 };
 
+
+// function to get weather data using coordinates
+const weatherDataByCoords = async (lat, lon, callback) => {
+    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${openWeatherMap.SECRET_KEY}&units=metric`;
+    const url = openWeatherMap.COORDS_BASE_URL +
+        "lat=" + encodeURIComponent(lat) +
+        "&lon=" + encodeURIComponent(lon) +
+        "&APPID=" + openWeatherMap.SECRET_KEY +
+        "&units=metric"; // Include units = metric for Celsius
+
+    console.log(url); // Debug log
+
+    try {
+        const response = await axios.get(url);
+        callback(false, response.data);
+    } catch (error) {
+        if (error.response && error.response.status === 401) {
+            callback(true, "Invalid API key. Please check your API key and try again.");
+        } else if (error.response && error.response.status === 404) {
+            callback(true, "Location not found. Please check your input.");
+        } else {
+            callback(true, "Unable to fetch data, please try again. " + error.message);
+        }
+    }
+};
+
+
 // Export the function so it can be used in other files
-module.exports = weatherData;
+module.exports = { weatherData, weatherDataByCoords };
